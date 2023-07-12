1f6e37ef52e31f486add1640a3500c3c6686e205
ìoxjadex <oharin765@gmail.com>
2023-07-12 14:20:48 +0900

    사진 불러오고 글자 씀 위치 조절해하면 거의 끝끝

--- danuri_JS.js
+++
@@ -1,8 +0,0 @@
-let today = new Date();   
-
-let hours = today.getHours(); // 시
-let minutes = today.getMinutes();  // 분
-let seconds = today.getSeconds();  // 초
-let milliseconds = today.getMilliseconds(); // 밀리초
-
-document.write(hours + ':' + minutes + ':' + seconds + ':' + milliseconds);
